d1e690dd41ede42d6421a9ab0fac4008d4436986
WarriorMind04 <josemiguel.guerrerojim@gmail.com>
2025-02-22 18:01:24 -0600

    Creación de formulario del Sign Up

---
+++ src/app/Components/signup.tsx
@@ -0,0 +1,98 @@
+export default function Registro() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://redpeg.com/wp-content/uploads/2022/01/Imagine-Dragons-Logo.png"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+            Create your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-white"
+              >
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  autoComplete="name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-400 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-white"
+              >
+                Email Address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-400 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-white"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="new-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-400 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-[#558C99] px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-[#DF5669] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-white">
+            Already have an account?{" "}
+            <a
+              href="#"
+              className="font-semibold text-[#558C99] hover:text-[#558C99]"
+            >
+              Sign in
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/SignUp/page.tsx
@@ -0,0 +1,11 @@
+import Registro from "../Components/signup";
+import Menu from "../Components/menu";
+
+export default function SignU() {
+  return (
+    <div className="bg-[url('https://wallpapercave.com/wp/wp14699777.jpg')] bg-cover bg-center min-h-screen">
+      <Menu />
+      <Registro />
+    </div>
+  );
+}
